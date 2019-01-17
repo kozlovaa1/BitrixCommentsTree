148b249a98669914be65153e38868a0f8d92a9ad
ak <a.kozlov@studioratio.ru>
2019-01-17 22:28:51 +0500

    Доработано формирование массива комментариев

--- local/templates/main/components/bitrix/news.list/comments_tree/template.php
+++ local/templates/main/components/bitrix/news.list/comments_tree/template.php
@@ -1,4 +1,4 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
@@ -13,38 +13,52 @@
 $this->setFrameMode(true);
 
 $map_items = array();
-foreach ($arResult["ITEMS"] as $arItem) {
+foreach ($arResult["ITEMS"] as $key => $arItem) {
 
     if ($arItem['PROPERTIES']['PARENT_COMMENT'] > 0) {
         $parent_id = $arItem['PROPERTIES']['PARENT_COMMENT']['VALUE'];
-        $map_items['' . $parent_id]['ID'] = $arResult["ITEMS"][$parent_id]['ID'];
-        $map_items['' . $parent_id]['NAME'] = $arResult["ITEMS"][$parent_id]['NAME'];
-        $map_items['' . $parent_id]['TIMESTAMP_X'] = $arResult["ITEMS"][$parent_id]['TIMESTAMP_X'];
-        $map_items['' . $parent_id]['PREVIEW_TEXT'] = $arResult["ITEMS"][$parent_id]['PREVIEW_TEXT'];
-        $map_items['' . $parent_id]['EDIT_LINK'] = $arResult["ITEMS"][$parent_id]['EDIT_LINK'];
-        $map_items['' . $parent_id]['DELETE_LINK'] = $arResult["ITEMS"][$parent_id]['DELETE_LINK'];
-        $map_items['' . $parent_id]['IBLOCK_ID'] = $arResult["ITEMS"][$parent_id]['IBLOCK_ID'];
-        $map_items['' . $parent_id]['CHILDREN'][] = array(
-            "ID" => $arItem['ID'],
-            "NAME" => $arItem['NAME'],
-            "TIMESTAMP_X" => $arItem['TIMESTAMP_X'],
-            "PREVIEW_TEXT" => $arItem['PREVIEW_TEXT'],
-            "PARENT_ID" => $arItem['PROPERTIES']['PARENT_COMMENT']['VALUE'],
-            "EDIT_LINK" => $arItem['EDIT_LINK'],
-            "DELETE_LINK" => $arItem['DELETE_LINK'],
-            "IBLOCK_ID" => $arItem['IBLOCK_ID'],
-        );
+        foreach ($arResult["ITEMS"] as $arParentItem) {
+            if ($arParentItem['ID'] == $parent_id) {
+                $map_items['' . $parent_id]['ID'] = $parent_id;
+                $map_items['' . $parent_id]['NAME'] = $arParentItem['NAME'];
+                $map_items['' . $parent_id]['TIMESTAMP_X'] = $arParentItem['TIMESTAMP_X'];
+                $map_items['' . $parent_id]['PREVIEW_TEXT'] = $arParentItem['PREVIEW_TEXT'];
+                //$map_items['' . $parent_id]['EDIT_LINK'] = $arParentItem['EDIT_LINK'];
+                //$map_items['' . $parent_id]['DELETE_LINK'] = $arParentItem['DELETE_LINK'];
+                //$map_items['' . $parent_id]['IBLOCK_ID'] = $arParentItem['IBLOCK_ID'];
+                $map_items['' . $parent_id]['CHILDREN'][] = array(
+                    "ID" => $arItem['ID'],
+                    "NAME" => $arItem['NAME'],
+                    "TIMESTAMP_X" => $arItem['TIMESTAMP_X'],
+                    "PREVIEW_TEXT" => $arItem['PREVIEW_TEXT'],
+                    "PARENT_ID" => $arItem['PROPERTIES']['PARENT_COMMENT']['VALUE'],
+                    //"EDIT_LINK" => $arItem['EDIT_LINK'],
+                    //"DELETE_LINK" => $arItem['DELETE_LINK'],
+                    //"IBLOCK_ID" => $arItem['IBLOCK_ID'],
+                );
+            }
+        }
+
+
     }
 
 }
 
 $arResult['MAP'] = $map_items;
 ?>
-<?echo'<pre>',print_r($arResult['MAP'], true),'</pre>'?>
 <div class="comments">
     <?
-    function outTree($arItems)
+
+    /**
+     * Обход массива с рекурсией для построения дерева комментариев
+     *
+     * @param array $arItems Подготовленный массив комментариев
+     *
+     * @param boolean $childrenFlag Показатель вложенности комментария
+     */
+    function outTree($arItems, $childrenFlag)
     {
+        echo '<pre>' . __FILE__ . ':' . __LINE__ . ':<br>' . print_r($childrenFlag, true) . '</pre>';
         if (isset($arItems)) {
             foreach ($arItems as $arItem) {
 
@@ -66,32 +80,30 @@ $arResult['MAP'] = $map_items;
                 );*/
 
                 if ($arItem['ID']) {
-                    //echo '<pre>' . __FILE__ . ':' . __LINE__ . ':<br>ID = ' . print_r($arItem['ID'], true) . '</pre>';
-                    //echo '<pre>' . __FILE__ . ':' . __LINE__ . ':<br>PARENT_ID = ' . print_r($arItem['PARENT_ID'], true) . '</pre>';?>
-                    <div class="media" id="<?//= $this->GetEditAreaId($arItem['ID']); ?>">
-                        <i class="fas fa-user-circle mr-3"></i>
-                        <div class="media-body">
-                            <h5 class="mt-0"><?= $arItem['NAME'] ?></h5>
-                            <? $value = CIBlockFormatProperties::DateFormat($arParams["ACTIVE_DATE_FORMAT"], MakeTimeStamp($arItem['TIMESTAMP_X'], CSite::GetDateFormat())); ?>
-                            <p class="comment-date"><?= $value ?></p>
-                            <p><?= $arItem['PREVIEW_TEXT'] ?></p>
-                            <a class="btn btn-outline-primary reply-button" data-commentid="<?= $arItem['ID'] ?>" href="#">Ответить</a>
-                            <?
+                    ?>
+                    <div class="media <? $childrenFlag == true ? 'mt-3' : ''; ?>" id="<?//= $this->GetEditAreaId($arItem['ID']); ?>">
+                    <i class="fas fa-user-circle mr-3"></i>
+                    <div class="media-body">
+                    <h5 class="mt-0"><?= $arItem['NAME'] ?></h5>
+                    <? $value = CIBlockFormatProperties::DateFormat($arParams["ACTIVE_DATE_FORMAT"], MakeTimeStamp($arItem['TIMESTAMP_X'], CSite::GetDateFormat())); ?>
+                    <p class="comment-date"><?= $value ?></p>
+                    <p><?= $arItem['PREVIEW_TEXT'] ?></p>
+                    <a class="btn btn-outline-primary reply-button" data-commentid="<?= $arItem['ID'] ?>" href="#">Ответить</a>
+                    <?
                 }
                 if (count($arItem['CHILDREN']) > 0) {
                     //Рекурсивно вызываем эту же функцию, но с новым $parent_id
-                    outTree($arItem['CHILDREN']);
+                    outTree($arItem['CHILDREN'], true);
                 }
-                if ($arItem['ID']) { ?>
-                        </div>
-                    </div>
+                ?>
+                </div>
+                </div>
                 <?
-                }
             }
         }
     }
 
-    //outTree($arResult['MAP']);
+    outTree($arResult['MAP'], false);
 
     ?>
 
